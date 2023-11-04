9d20b153bb9e005fe07c2eb645656d6a28f68992
Ramiro Correa <98496459+ramicorrea21@users.noreply.github.com>
2023-11-04 15:16:29 +0000

    first commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,15 +4,37 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from werkzeug.security import generate_password_hash, check_password_hash
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/singup', methods=['POST'])
+def singup():
+    body = request.json
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    email = body.get('email')
+    password = body.get('password')
 
-    return jsonify(response_body), 200
+    if email is None or password is None:
+        return({"message":"Bad credentials"}), 400
+    
+    User = User.query.filter_by(email=email).one_or_none()
+    if User is not None:
+        return({"message":"user already exists"})
+    else:
+        password = generate_password_hash(password)
+        add_user = User(email=email, password=password)
+        db.session.add(add_user)
+
+    try:
+        db.session.commit()
+        return({"message":"user created"}), 201
+    except Exception as error:
+        db.session.rollback()
+        return({"error":f"{error}"})
+    
+
+@api.route('/health_check', methods=['GET'])
+def health_check():
+    return jsonify({"message":"good"})
