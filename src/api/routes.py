a451b85c6e245573b053d075cc21babdb7cd40b6
Ramiro Correa <98496459+ramicorrea21@users.noreply.github.com>
2023-11-05 00:07:51 +0000

    login and singup endpoints

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,23 +5,24 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
 
 api = Blueprint('api', __name__)
 
-
+#Signup: The user must be able to pick its email, any password and submit the form, a new user must be created in the database and the user must be redirected to the login form afterwards.
 @api.route('/singup', methods=['POST'])
 def singup():
     body = request.json
-
-    email = body.get('email')
-    password = body.get('password')
-
-    if email is None or password is None:
-        return({"message":"Bad credentials"}), 400
+    email = body.get("email")
+    password = body.get("password")
     
-    User = User.query.filter_by(email=email).one_or_none()
-    if User is not None:
-        return({"message":"user already exists"})
+    if email is None or password is None:
+        return({"message":"bad credentials"})
+
+    user = User.query.filter_by(email=email).one_or_none()
+
+    if user is not None:
+        return({"message":"user already exists"}), 400
     else:
         password = generate_password_hash(password)
         add_user = User(email=email, password=password)
@@ -29,12 +30,25 @@ def singup():
 
     try:
         db.session.commit()
-        return({"message":"user created"}), 201
+        return jsonify({"message":"user created"}), 201
     except Exception as error:
         db.session.rollback()
-        return({"error":f"{error}"})
+        return({"error":f"{error}"}), 500
+
+@api.route('/login', methods=['POST'])
+def login():
+    body = request.json
+    email = body.get("email")
+    password = body.get("password")
+
+    user_exists = User.query.filter_by(email=email).one_or_none()
+    if user_exists is None:
+        return({"message":"bad credentials"}), 400
+    else:
+        if check_password_hash(user_exists.password, password):
+            token = create_access_token(identity=user_exists.id)
+            return({"token":f"{token}"})
+        else:
+            return({"message":"bad credentials"}), 400
     
 
-@api.route('/health_check', methods=['GET'])
-def health_check():
-    return jsonify({"message":"good"})
